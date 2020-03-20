cbc7e177881479523f774319d5dec0ee96bfd4c3
Elvy_kiung <kiungh22@gmail.com>
2020-03-20 14:03:18 -0700

    set up footer component

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,10 +3,12 @@ import PropTypes from "prop-types"
 import "./bootstrap.min.css"
 import "./layout.css"
 import Navbar from "./Globals/Navbar"
+import Footer from "./Globals/Footer"
+
 const Layout = ({ children }) => (
   <>
     <Navbar />
-    {children}{" "}
+    {children} <Footer />
   </>
 )
 

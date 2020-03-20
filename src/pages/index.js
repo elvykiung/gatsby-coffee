baad1c6f94222df68eb9bbed233cdaacd1b951c4
Elvy_kiung <kiungh22@gmail.com>
2020-03-20 13:56:23 -0700

    create info section for about link

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,8 @@ import { Link, graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import BackgroundSection from "../components/Globals/BackgroundSection"
+import Info from "../components/Home/Info"
+
 const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" />
@@ -11,6 +13,7 @@ const IndexPage = ({ data }) => (
       styleClass="default-background"
       title="regular Joe's"
     />
+    <Info></Info>
   </Layout>
 )
 
